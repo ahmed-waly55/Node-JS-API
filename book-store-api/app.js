817fccb2fb943bc8a554ccb596239d37a4841bbd
ahmed waly <ahmedwaly074@gmail.com>
2026-02-05 19:24:28 +0200

    learn GET Method Api and install nodemon

--- book-store-api/app.js
+++ book-store-api/app.js
@@ -21,11 +21,21 @@ const books = [
 // HTTP Methods / Verb 
 // GET Method 
 app.get("/", (req, res) => {
-    res.send("hello API")
+    res.send("hello Express API")
 });
 app.get("/api/books", (req, res) => {
     // res.json(JSON.stringify(books))
-    res.json(books)
+    res.status(200).json(books)
+})
+app.get("/api/books/:id", (req, res) => {
+    const book = books.find(b => b.id === parseInt(req.params.id));
+    if (book) {
+        res.status(200).json(book);
+    } else {
+        res.status(404).json({
+            massage: "book Not Found"
+        })
+    }
 })
 // POST Method 
 // app.post();
