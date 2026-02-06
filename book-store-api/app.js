0043339ebc716253d3ce5531ded845499ee2ce6c
ahmed waly <ahmedwaly074@gmail.com>
2026-02-06 15:10:57 +0200

    added route

--- book-store-api/app.js
+++ book-store-api/app.js
@@ -1,69 +1,15 @@
 const express = require("express");
-const joi = require("joi")
+const booksPath = require("./routes/books")
+
 // init App 
 const app = express();
+
+// Applay Middlewares 
 app.use(express.json())
-const books = [
-    {
-        id: 1,
-        name: "book 1"
-    },
-    {
-        id: 2,
-        name: "book 2"
-    },
-    {
-        id: 3,
-        name: "book 3"
-    },
-]
 
-// HTTP Methods / Verb 
-// GET Method 
-app.get("/", (req, res) => {
-    res.send("hello Express API")
-});
-app.get("/api/books", (req, res) => {
-    const schema = joi.object({
-        name: joi.string().trim().min(3).max(50).required(),
-        price: max(10).require()
-    });
+// Routes 
+app.use("/api/books", booksPath)
 
-    const { error } = schema.validate(req.body);
-    if (error) {
-        return res.status(400).json(error)
-    }
-    // res.json(JSON.stringify(books))
-    res.status(200).json(books)
-})
-app.get("/api/books/:id", (req, res) => {
-    const book = books.find(b => b.id === parseInt(req.params.id));
-    if (book) {
-        res.status(200).json(book);
-    } else {
-        res.status(404).json({
-            massage: "book Not Found"
-        })
-    }
-})
-// POST Method 
-app.post("/api/books", (req, res) => {
-    if (!req.body.name || req.body.name.length > 3) {
-        return res.status(400).json("Name Is Required and must be more than char")
-    }
-    const book = {
-        id: books.length + 1,
-        name: req.body.name,
-        price: req.body.price
-    }
-    books.push(book)
-    res.status(201).json(book)
-});
-// PUT Method 
-// app.put();
-// DELETE Method 
-// app.delete();
 
-// Renning the Server 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`PORT NUMBER Is ${PORT}`));
